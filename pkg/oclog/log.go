3ae70f89198615ebad8d153001e1367db83e0437
mariusbertram <10505884+mariusbertram@users.noreply.github.com>
2026-07-04 13:35:32 +0000

    refactor(log): replace fmt.Printf with structured logging
    Replaced plain `fmt.Printf` and `fmt.Println` in `pkg/oclog/log.go` with structured logging using `sigs.k8s.io/controller-runtime/pkg/log`. Removed custom timestamp generation since structured loggers handle this natively.
    
    This improves code consistency within the Kubernetes operator context and facilitates log scraping.

--- pkg/oclog/log.go
+++ pkg/oclog/log.go
@@ -2,21 +2,20 @@ package oclog
 
 import (
 	"fmt"
-	"time"
+	"strings"
+
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
-
-const tsFormat = "2006-01-02T15:04:05Z07:00"
-
-func ts() string {
-	return time.Now().Format(tsFormat)
-}
 
 // Printf writes a timestamped log line to stdout.
 func Printf(format string, args ...any) {
-	fmt.Printf("["+ts()+"] "+format, args...)
+	msg := fmt.Sprintf(format, args...)
+	msg = strings.TrimSuffix(msg, "\n")
+	log.Log.Info(msg)
 }
 
 // Println writes a timestamped log line to stdout.
 func Println(msg string) {
-	fmt.Println("[" + ts() + "] " + msg)
+	msg = strings.TrimSuffix(msg, "\n")
+	log.Log.Info(msg)
 }
